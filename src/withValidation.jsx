8d64390710f3595ab72acf131a71541640b73814
kenta88 <solinvictus.adc@gmail.com>
2017-07-05 14:13:44 +0200

    fix

--- src/withValidation.jsx
+++ src/withValidation.jsx
@@ -226,6 +226,7 @@ export default function withValidation(configuration: Object | ReactClass<any>) 
                         isValid: null,
                         fields,
                     },
+                    isButtonDisabled: true,
                 });
             }
 
@@ -234,6 +235,7 @@ export default function withValidation(configuration: Object | ReactClass<any>) 
                 this.resetValidation();
                 this.setState({
                     model: this.state.initialModel,
+                    isButtonDisabled: true,
                 });
 
                 if (this.state.validateOn !== 'submit') {
